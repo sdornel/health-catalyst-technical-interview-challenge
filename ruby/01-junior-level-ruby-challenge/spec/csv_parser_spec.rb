ca3b9caba5733e9a249b75bdde06a561e5d16f86
kaidornel <kai_dornel@hotmail.com>
2020-09-27 20:15:32 -0400

    added validation in case csv parser got nil nilclass error

--- ruby/01-junior-level-ruby-challenge/spec/csv_parser_spec.rb
+++ ruby/01-junior-level-ruby-challenge/spec/csv_parser_spec.rb
@@ -3,12 +3,27 @@
 require_relative '../src/csv_parser'
 require 'tempfile'
 
+require 'pry'
+
 RSpec.describe CSVParser do
   let(:csv_file) { Tempfile.new.tap { |f| f.write(csv_contents) }.tap(&:rewind) }
   let(:filepath) { csv_file.path }
 
   describe '.header_row' do
     subject { described_class.header_row(filepath) }
+
+    # test would be something like:
+    # expect CSV.parse(File.new(filepath).each_line.first).first to equal nil
+    # how to fix it?
+    # context 'when the csv file is missing headers' do
+    #   let(:csv_contents) { ",,,\nfirstValue,secondValue" } 
+    #   # binding.pry
+    #   it "does not return nil" do
+    #     expect(CSV.parse(("\ntext1").each_line.first).first).to eq nil
+    #   # CSV.parse(File.new(filepath).each_line.first).first.map(&:downcase)
+    #   end
+    # end
+
 
     context 'when the csv file has data' do
       let(:csv_contents) { "firstHeader,secondHeader\nfirstValue,secondValue" }
--- ruby/01-junior-level-ruby-challenge/src/csv_parser.rb
+++ ruby/01-junior-level-ruby-challenge/src/csv_parser.rb
@@ -3,8 +3,26 @@
 require 'csv'
 require 'pry'
 
+# require_relative 'data.csv'  
+
 module CSVParser
   def self.header_row(filepath)
-    CSV.parse(File.new(filepath).each_line.first).first.map(&:downcase)
+    binding.pry
+    parse = CSV.parse(File.new(filepath).each_line.first).first
+    if parse.nil?
+      parse = ["headers missing!"]
+      parse.first.map(&:downcase)
+    else
+      CSV.parse(File.new(filepath).each_line.first).first.map(&:downcase)
+    end
+
   end
 end
+# NoMethodError: undefined method `map' for nil:NilClass
+# I know this occurs when CSV.parse(File.new(filepath).each_line.first).first returns nil
+
+# .nil? could be useful
+
+# test would be something like:
+# expect CSV.parse(File.new(filepath).each_line.first) to equal nil
+# how to fix it?
