4008e61dfde34f420b99f3086297f974c4d25cc3
kaidornel <kai_dornel@hotmail.com>
2020-09-30 16:56:22 -0400

    sql done

--- sql/01-junior-level-sql-challenge/feedback.md
+++ sql/01-junior-level-sql-challenge/feedback.md
@@ -1,1 +1,3 @@
 Optionally, leave any feedback - positive, negative, or otherwise about this challenge.
+
+Wasn't sure if you wanted all the detail for providers, or just an ID. I tried to do both just in case
--- sql/01-junior-level-sql-challenge/queries.sql
+++ sql/01-junior-level-sql-challenge/queries.sql
@@ -1,1 +1,25 @@
 -- Record SQL Queries here
+
+--    1. All patients, and the family name of their provider
+SELECT patients.*, providers.family_name FROM patients JOIN providers ON patients.provider_external_id = providers.external_id;
+-- SELECT patients.id, providers.family_name FROM patients JOIN providers ON patients.provider_external_id = providers.external_id;
+
+--    2. All providers and the count of their patients
+SELECT providers.*, 
+(SELECT COUNT(*) 
+FROM patients WHERE patients.provider_external_id = providers.external_id) AS count
+FROM providers;
+-- SELECT providers.external_id, COUNT(patients.provider_external_id) AS count FROM providers
+-- LEFT JOIN patients ON patients.provider_external_id = providers.external_id
+-- GROUP BY providers.external_id;
+
+--    3. All providers that don't have any patients
+SELECT providers.*
+FROM providers
+WHERE providers.external_id NOT IN ( SELECT patients.provider_external_id FROM patients);
+
+--    4. All providers and a count of all their patients over 55 years of age
+SELECT providers.*,
+(SELECT COUNT(*) 
+FROM patients WHERE patients.provider_external_id = providers.external_id AND date_of_birth < now() - '55 years'::interval) 
+FROM providers;
