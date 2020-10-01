36bc7419a90562db3db9e871aece1ba1ca0f4b3d
kaidornel <kai_dornel@hotmail.com>
2020-10-01 09:40:53 -0400

    ruby done

--- ruby/01-junior-level-ruby-challenge/spec/csv_parser_spec.rb
+++ ruby/01-junior-level-ruby-challenge/spec/csv_parser_spec.rb
@@ -14,11 +14,7 @@ RSpec.describe CSVParser do
   describe '.header_row' do
     subject { described_class.header_row(filepath) }
 
-    # test would be something like:
-    # expect CSV.parse(File.new(filepath).each_line.first).first to equal nil
-
-    # how to fix it?
-    context 'when the header includes commas' do # is the bug happening because parsing non-csv file?
+    context 'when the header includes commas' do
       let(:csv_contents) { "a,," } 
       it "give NoMethodError: undefined method `downcase' for nil:NilClass" do
         expect{
@@ -28,14 +24,14 @@ RSpec.describe CSVParser do
     end
 
 
-    # context 'when the csv file has data' do
-    #   let(:csv_contents) { "firstHeader,secondHeader\nfirstValue,secondValue" }
-    #   it { is_expected.to eq %w[firstheader secondheader] }
-    # end
+    context 'when the csv file has data' do
+      let(:csv_contents) { "firstHeader,secondHeader\nfirstValue,secondValue" }
+      it { is_expected.to eq %w[firstheader secondheader] }
+    end
 
-    # context 'when the csv file only has headers' do
-    #   let(:csv_contents) { "firstHeader,secondHeader\n" }
-    #   it { is_expected.to eq %w[firstheader secondheader] }
-    # end
+    context 'when the csv file only has headers' do
+      let(:csv_contents) { "firstHeader,secondHeader\n" }
+      it { is_expected.to eq %w[firstheader secondheader] }
+    end
   end
 end
