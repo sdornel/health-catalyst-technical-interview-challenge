dd9f51dad2a4782c2ee9c1283e7cfb3e72a03d07
kaidornel <kai_dornel@hotmail.com>
2020-10-01 09:46:18 -0400

    fixed export bug in customer

--- rails/01-junior-level-rails-challenge/patient-app/app/models/customer.rb
+++ rails/01-junior-level-rails-challenge/patient-app/app/models/customer.rb
@@ -19,7 +19,7 @@ class Customer < ApplicationRecord
                 end
             end
             start = Time.now
-            Export.create(customer_id: customer_id, export_status: "Complete", number_records: patients.length, created_at: start, export_duration_in_mins: ((Time.now - start)/60), export_ended: DateTime.now )
+            export = Export.create(customer_id: customer_id, export_status: "Complete", number_records: patients.length, created_at: start, export_duration_in_mins: ((Time.now - start)/60), export_ended: DateTime.now )
             if export.id == nil || export.number_records == nil || export.created_at == nil || export.export_ended == nil || export.export_duration_in_mins == nil || export.customer_id == nil
                 Export.last.update(export_status: "Failed (check other columns)")
             end
